1df0af8eefeec797de9e886b915bd5995edc7009
MaxDiaz-CS <put.your.email.here@my.simpson.edu>
2021-09-02 20:50:39 -0500

    Work on lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,10 @@
+print("John Wayne was born in Winterset.")
+print("Napoleon said that \"History is a set of lies agreed upon.\"")
+print("Tanner told us,\"We will have a meeting later tonight at six o clock.\"")
+print("Caleb's\nfavorite\nseason\nis\nAutumn.")
+print("Davy Crocket\ndied fighting\nin the Alamo.")
+print("""Todd asks his parents for money.
+Todd went to the supermarket to buy some chocolate.
+On the way home, he found a stray pup.
+He brought him home and named him Bobby.
+""")
