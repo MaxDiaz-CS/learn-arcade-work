e4b29a1c0e2c6d5de50736a531d5d04b956ee171
MaxDiaz-CS <89961169+MaxDiaz-CS@users.noreply.github.com>
2021-09-16 14:35:37 -0500

    Add files via upload

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,171 @@
+import arcade
+
+# Open a window
+arcade.open_window(800, 600, "Rome")
+
+# Set the background color
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+
+# Get ready to draw
+arcade.start_render()
+
+# Ground
+arcade.draw_lrtb_rectangle_filled(0, 800, 150, 1, arcade.csscolor.DIM_GRAY)
+arcade.draw_lrtb_rectangle_filled(0, 800, 100, 1, arcade.csscolor.GREEN)
+
+# Colosseum
+arcade.draw_lrtb_rectangle_filled(100, 400, 325, 150, arcade.csscolor.NAVAJO_WHITE)
+arcade.draw_ellipse_filled(390, 238, 52, 179,
+                           arcade.color.NAVAJO_WHITE, 180)
+arcade.draw_ellipse_filled(110, 238, 52, 179,
+                           arcade.color.NAVAJO_WHITE, 180)
+# Top Windows of the Colosseum
+arcade.draw_arc_filled(400, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(370, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(340, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(310, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(280, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(250, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(220, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(190, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(160, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(130, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(100, 280, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+
+# Top Windows of the Colosseum
+arcade.draw_arc_filled(400, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(370, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(340, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(310, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(280, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(250, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(220, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(190, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(160, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(130, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(100, 250, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+
+# Top Windows of the Colosseum
+arcade.draw_arc_filled(400, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(370, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(340, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(310, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(280, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(250, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(220, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(190, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(160, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(130, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(100, 220, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+
+# Top Windows of the Colosseum
+arcade.draw_arc_filled(400, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(370, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(340, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(310, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(280, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(250, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(220, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(190, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(160, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(130, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+arcade.draw_arc_filled(100, 190, 20, 40,
+                       arcade.color.BLACK, 0, 180, 0)
+
+# flags poles
+arcade.draw_line(100, 325, 100, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(130, 325, 130, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(160, 325, 160, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(190, 325, 190, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(220, 325, 220, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(250, 325, 250, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(250, 325, 250, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(280, 325, 280, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(310, 325, 310, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(340, 325, 340, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(370, 325, 370, 345, arcade.color.WOOD_BROWN, 4)
+arcade.draw_line(400, 325, 400, 345, arcade.color.WOOD_BROWN, 4)
+
+# flags
+arcade.draw_lrtb_rectangle_filled(100, 117, 345, 335, arcade.csscolor.DARK_RED)
+arcade.draw_lrtb_rectangle_filled(130, 147, 345, 335, arcade.csscolor.GOLDENROD)
+arcade.draw_lrtb_rectangle_filled(160, 177, 345, 335, arcade.csscolor.DARK_RED)
+arcade.draw_lrtb_rectangle_filled(190, 207, 345, 335, arcade.csscolor.GOLDENROD)
+arcade.draw_lrtb_rectangle_filled(220, 237, 345, 335, arcade.csscolor.DARK_RED)
+arcade.draw_lrtb_rectangle_filled(250, 267, 345, 335, arcade.csscolor.GOLDENROD)
+arcade.draw_lrtb_rectangle_filled(280, 297, 345, 335, arcade.csscolor.DARK_RED)
+arcade.draw_lrtb_rectangle_filled(310, 327, 345, 335, arcade.csscolor.GOLDENROD)
+arcade.draw_lrtb_rectangle_filled(340, 357, 345, 335, arcade.csscolor.DARK_RED)
+arcade.draw_lrtb_rectangle_filled(370, 387, 345, 335, arcade.csscolor.GOLDENROD)
+arcade.draw_lrtb_rectangle_filled(400, 417, 345, 335, arcade.csscolor.DARK_RED)
+
+# Doors
+arcade.draw_lrtb_rectangle_filled(120, 137, 165, 149, arcade.csscolor.BEIGE)
+arcade.draw_lrtb_rectangle_filled(137, 154, 165, 149, arcade.csscolor.BEIGE)
+
+arcade.draw_lrtb_rectangle_filled(220, 237, 165, 149, arcade.csscolor.BEIGE)
+arcade.draw_lrtb_rectangle_filled(237, 254, 165, 149, arcade.csscolor.BEIGE)
+
+arcade.draw_lrtb_rectangle_filled(330, 347, 165, 149, arcade.csscolor.BEIGE)
+arcade.draw_lrtb_rectangle_filled(347, 364, 165, 149, arcade.csscolor.BEIGE)
+
+# Temple
+arcade.draw_lrtb_rectangle_filled(500, 700, 325, 150, arcade.csscolor.NAVAJO_WHITE)
+arcade.draw_lrtb_rectangle_filled(500, 700, 255, 250, arcade.csscolor.WHITE)
+arcade.draw_lrtb_rectangle_filled(500, 700, 325, 320, arcade.csscolor.WHITE)
+arcade.draw_lrtb_rectangle_filled(500, 550, 250, 150, arcade.csscolor.WHITE)
+arcade.draw_lrtb_rectangle_filled(650, 700, 250, 150, arcade.csscolor.WHITE)
+arcade.draw_lrtb_rectangle_filled(575, 625, 200, 150, arcade.csscolor.BLACK)
+arcade.draw_lrtb_rectangle_filled(585, 625, 315, 275, arcade.csscolor.BLACK)
+arcade.draw_lrtb_rectangle_filled(510, 550, 315, 275, arcade.csscolor.BLACK)
+arcade.draw_lrtb_rectangle_filled(655, 695, 315, 275, arcade.csscolor.BLACK)
+arcade.draw_triangle_filled(612, 525, 490, 320, 710, 320, arcade.csscolor.DARK_RED)
+
+# Finish drawing
+arcade.finish_render()
+
+# Keep the window up until someone closes it.
+arcade.run()
