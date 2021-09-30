4d15be0bbe29a64b3cb422b31049a151c99ccc0b
MaxDiaz-CS <put.your.email.here@my.simpson.edu>
2021-09-30 13:52:54 -0500

    Work on lab 3

---
+++ Lab 04 - Camel/lab_04.py
@@ -0,0 +1,11 @@
+# Camel 4
+# Still not done going to resubmit by friday night along with CIS 120
+
+import random
+
+# Introduction
+print("Welcome to Camel!")
+print("You have stolen a prized camel from the Khan with goal of selling it.")
+print("The Khan's men want their Khan's prized camel back and are chasing you across the steppes!")
+print("You have to survive the trek across the steppes until you get to Song Kingdom")
+print()
